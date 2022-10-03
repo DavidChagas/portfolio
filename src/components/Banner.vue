de34691477d0193ff1b918499a5de5eb1c96685d
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2022-10-02 21:38:42 -0300

    banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="banner">
+        <img class="img-fluid" alt="Banner" src="../assets/banner.jpg">
+        <div class="lamina"></div>
+        <div class="eu">
+        <div class="foto"></div>
+        <div class="nome">David Chagas</div>
+        <div class="descricao">Desenvolvedor Web</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<style lang="scss">
+.banner{
+    position: relative;
+
+    .lamina{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.549);
+        z-index: 0;
+    }
+    .eu{
+        position: absolute;
+        top: 20%;
+        left: calc(50% - 150px);
+
+        width: 300px;
+        padding: 20px;
+
+        text-align: center;
+
+        .foto{
+        width: 200px;
+        height: 200px ;
+
+        margin: 0 auto 20px auto;
+
+        border: 2px solid white;
+        border-radius: 50%;
+
+        background-image: url('../assets/myself.jpg');
+        background-size: cover;
+
+        z-index: 9999;
+        }
+
+        .nome{
+        font-weight: bold;
+        font-size: 30px;
+        color: #f05a59;
+        }
+        .descricao{
+        font-size: 14px;
+        color: rgba(255, 255, 255, 0.755);
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner/>
+    <Sobre/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '@/components/Banner.vue'
+import Sobre from '@/components/Sobre.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Banner,
+    Sobre
   }
 }
 </script>
